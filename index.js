aa5faecb6e2e3830590ae166fba4d7bc5cf7881f
Galia <glyhdwnh556@gmail.com>
2023-05-24 13:40:22 +0300

    third commit making some changes and uplod files

--- index.js
+++ index.js
@@ -5,5 +5,23 @@ function Log_in() {
   }
 //main page
 function main(){
-  window.location.assign("mainpage.html")
+    console.log('hey')
+    window.location.assign("mainpage.html")
 }
+
+function openCity(cityName,elmnt,color) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+  }
+  document.getElementById(cityName).style.display = "block";
+  elmnt.style.backgroundColor = color;
+
+}
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
